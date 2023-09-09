276da8b343b8576921383ce58346a5f6bfc32e3f
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-09-09 12:38:00 -0300

    update

--- routes/pages.php
+++ routes/pages.php
@@ -15,3 +15,21 @@ $rota->get('/produtos', [
         return new Response(200, Pages\Produtos::getProdutos());
     }
 ]);
+
+$rota->get('/fundadores', [
+    function() {
+        return new Response(200, Pages\Fundadores::getFundadores());
+    }
+]);
+
+$rota->get('/sobre-nos', [
+    function() {
+        return new Response(200, Pages\SobreNos::getSobreNos());
+    }
+]);
+
+$rota->get('/area-do-cliente', [
+    function() {
+        return new Response(200, Pages\AreaDoCliente::getAreaDoCliente());
+    }
+]);
--- src/Controllers/Pages/SobreNos.php
+++ src/Controllers/Pages/SobreNos.php
@@ -4,7 +4,7 @@ namespace Abwel\Phplace\Controllers\Pages;
 
 use Abwel\Phplace\Utils\ViewRenderer;
 
-class SobreNos extends Page {
+class SobreNos {
     public static function getSobreNos() {
         $sobreNosContent = ViewRenderer::render('pages/sobre-nos');
         return Page::getPageContent('Sobre Nos', $sobreNosContent);
