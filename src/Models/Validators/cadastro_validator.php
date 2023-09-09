64b9545ea3896d3c662bb6e6bb229b7386981a31
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-09-09 20:03:05 -0300

    update

--- src/Models/Validators/cadastro_validator.php
+++ src/Models/Validators/cadastro_validator.php
@@ -3,10 +3,12 @@
 require_once __DIR__ . '/../../../src/Models/Utils/sanitizer/vendor/autoload.php';
 require_once __DIR__ . '/../../../src/Models/Entity/User.php';
 require_once __DIR__ . '/../../../src/Models/Storage/LocalDatabase.php';
+require_once __DIR__ . '/../../../src/Models/Utils/sanitizer/src/Sanitizers/DatabaseSanitizer.php';
 
 use Brc\Inspector\Validators\EmailValidator;
 use Brc\Inspector\Validators\NameValidator;
 use Brc\Inspector\Validators\PasswordValidator;
+use \Abwel\Phplace\Models\Utils\sanitizer\src\Sanitizers\DatabaseSanitizer;
 use Abwel\Phplace\Models\Entity\User;
 
 if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password'])) {
@@ -15,7 +17,7 @@ if (!isset($_POST['name']) || !isset($_POST['email']) || !isset($_POST['password
 }
 
 
-$name     = $_POST['name'];
+$name     = DatabaseSanitizer::sanitize($_POST['name']);
 $email    = $_POST['email'];
 $password = $_POST['password'];
 
@@ -32,6 +34,11 @@ if (!EmailValidator::validate($email)) {
 
 if (!PasswordValidator::validate($password)) {
     header('Location: /phplace-market/cadastro?error=senha-invalida');
+    exit;
+}
+
+if(\Abwel\Phplace\Models\Storage\LocalDatabase::userExists($email, $password)) {
+    header('Location: /phplace-market/cadastro?error=usuario-ja-cadastrado');
     exit;
 }
 
